5d56937d6e9dcafe94c1f3b3c4e1a9b01420167a
OBonk <olli.brew@outlook.com>
2021-11-11 14:16:50 +0000

    loads email file as html

--- deploy.py
+++ deploy.py
@@ -2,8 +2,8 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-sender_email = "my@gmail.com"
-receiver_email = "your@gmail.com"
+sender_email = "olliscloner@gmail.com"
+receiver_email = input("Enter target email:")
 password = input("Type your password and press enter:")
 
 message = MIMEMultipart("alternative")
@@ -12,31 +12,15 @@ message["From"] = sender_email
 message["To"] = receiver_email
 
 # Create the plain-text and HTML version of your message
-text = """\
-Hi,
-How are you?
-Real Python has many great tutorials:
-www.realpython.com"""
-html = """\
-<html>
-  <body>
-    <p>Hi,<br>
-       How are you?<br>
-       <a href="http://www.realpython.com">Real Python</a> 
-       has many great tutorials.
-    </p>
-  </body>
-</html>
-"""
+with open("Email.html","r") as f:
+    html=f.readlines()
 
 # Turn these into plain/html MIMEText objects
-part1 = MIMEText(text, "plain")
-part2 = MIMEText(html, "html")
+part = MIMEText(html, "html")
 
 # Add HTML/plain-text parts to MIMEMultipart message
 # The email client will try to render the last part first
-message.attach(part1)
-message.attach(part2)
+message.attach(part)
 
 # Create secure connection with server and send email
 context = ssl.create_default_context()
